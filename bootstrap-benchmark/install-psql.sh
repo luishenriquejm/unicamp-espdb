740a9d76cf0700a6bf40174a1c746a41c97e659f
luishenriquejm <luis.henrique.jm@hotmail.com>
2026-05-17 20:01:44 +0200

    update

--- bootstrap-benchmark/install-psql.sh
+++ bootstrap-benchmark/install-psql.sh
@@ -6,6 +6,7 @@ v_PG_VERSION=$1
 dnf install -y tree
 
 # repo 12
+if [ $v_PG_VERSION -eq 12 ]; then
 cat << EOF > /etc/yum.repos.d/pgdg-12.repo
 [pgdg12-archive]
 name=PostgreSQL 12 RPMs for RHEL/Rocky Linux/AlmaLinux 9.0
@@ -14,8 +15,10 @@ enabled=1
 gpgcheck=1
 gpgkey=https://yum.postgresql.org/keys/PGDG-RPM-GPG-KEY-RHEL
 EOF
+fi
 
 # repo 13
+if [ $v_PG_VERSION -eq 13 ]; then
 cat << EOF > /etc/yum.repos.d/pgdg-13.repo
 [pgdg13-archive]
 name=PostgreSQL 13 RPMs for RHEL/Rocky Linux/AlmaLinux 9.0
@@ -24,8 +27,11 @@ enabled=1
 gpgcheck=1
 gpgkey=https://yum.postgresql.org/keys/PGDG-RPM-GPG-KEY-RHEL
 EOF
+fi
 
-# repo 14, 15, 16, 17, 18
+# repo 14, 15, 16, 17, 18j
+if [ $v_PG_VERSION -ge 14 ]; then
 dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+fi
 
 dnf install -y postgresql${v_PG_VERSION}
